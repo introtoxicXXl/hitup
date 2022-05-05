b67f5db32b1387af094200a5146913bccfc5b827
introtoxicXXl <minhajulabedin648@gmail.com>
2022-05-06 04:18:57 +0600

    sign in code write

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,22 +1,62 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
+import auth from '../../firebase.init';
 import logIn from '../../img/login.svg';
+import Loading from '../Loading/Loading';
 import './SignIn.css';
 
 const SignIn = () => {
   const [validated, setValidated] = useState(false);
+  const emailRef = useRef('');
+  const passwordRef = useRef('');
+  const navigate = useNavigate();
+  const location = useLocation();
+  let from = location.state?.from?.pathname || "/";
+  let errorMsg;
+  const [
+    signInWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useSignInWithEmailAndPassword(auth);
+  const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
 
   const handleSubmit = (event) => {
+    event.preventDefault();
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
-      event.preventDefault();
       event.stopPropagation();
     }
-
     setValidated(true);
-  };
 
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+
+    signInWithEmailAndPassword(email, password);
+  };
+  if (user) {
+    navigate(from, { replace: true });
+  }
+  if (error) {
+    errorMsg = <p className='text-danger'>Error: {error.message}</p>
+  }
+  if(loading){
+    <Loading></Loading>
+  }
+
+  const resetPassword = async () => {
+    const email = emailRef.current.value;
+    if (email) {
+      await sendPasswordResetEmail(email);
+      toast('Email has been sent');
+    }
+    else {
+      toast('Please Enter Your Email Address');
+    }
+  }
 
 
 
@@ -30,16 +70,16 @@ const SignIn = () => {
           <Form className='w-75' onSubmit={handleSubmit} validated={validated} noValidate>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" required/>
+              <Form.Control type="email" ref={emailRef} placeholder="Enter email" required />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" required/>
+              <Form.Control type="password" ref={passwordRef} placeholder="Password" required />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
+            {errorMsg}
             <p>New in HitUp? <Link className='signin-link' to='/signup'>Sign Up</Link> </p>
+            <p>Forget Password? <button className='btn btn-link text-warning pe-auto text-decoration-none' onClick={resetPassword}>Reset Password</button> </p>
+            <ToastContainer/>
             <Button variant="outline-primary" type="submit">
               Sign In
             </Button>
