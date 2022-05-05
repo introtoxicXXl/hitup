ae11406b08d07b51b7737443ccb99c0cb2c471f6
introtoxicXXl <minhajulabedin648@gmail.com>
2022-05-06 02:42:55 +0600

    loading components added

--- src/components/RequireAuth/RequireAuth.js
+++ src/components/RequireAuth/RequireAuth.js
@@ -1,17 +1,17 @@
-// import React from 'react';
-// import auth from '../../firebase.init';
-// import { Navigate, useLocation } from 'react-router-dom';
-// import { useAuthState } from 'react-firebase-hooks/auth';
+import React from 'react';
+import auth from '../../firebase.init';
+import { Navigate, useLocation } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 
-// function RequireAuth({ children }) {
-//   const [user] = useAuthState(auth);
-//   const location = useLocation();
+function RequireAuth({ children }) {
+  const [user] = useAuthState(auth);
+  const location = useLocation();
 
-//   if (!user) {
-//     return <Navigate to="/login" state={{ from: location }} replace />;
-//   }
-//   return children;
-// }
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+  return children;
+}
 
-// export default RequireAuth;
+export default RequireAuth;
